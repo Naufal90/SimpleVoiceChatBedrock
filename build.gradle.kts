9a997af8f503d9a55f9a9f770f7c06d97e6c06a8
Naufal90 <45502745+Naufal90@users.noreply.github.com>
2025-01-06 13:47:38 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,19 @@
 repositories {
     maven {
+        url = uri("plugins {
+    kotlin("jvm") version "1.8.10"
+}
+
+repositories {
+    mavenCentral()
+    maven {
         url = uri("https://papermc.io/repo/repository/maven-public/")
+    }
+}
+
+dependencies {
+    implementation("io.papermc.paper:paper-api:1.16.5-R0.1-SNAPSHOT")
+}")
     }
 }
 
