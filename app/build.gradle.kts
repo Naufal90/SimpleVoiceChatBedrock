dd772962c5d51e21f03f7ff5f2655d2cc9fce458
Naufal90 <45502745+Naufal90@users.noreply.github.com>
2025-01-07 08:28:23 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,7 +33,6 @@ dependencies {
     implementation("androidx.core:core-ktx:1.10.1") // Dependensi tambahan yang umum digunakan
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
-    implementation("com.android.tools.build:gradle:8.1.0")
     implementation("de.maxhenkel.voicechat:voicechat-api:2.1.12") // Dependensi untuk VoiceChat API
     compileOnly("io.papermc.paper:paper-api:1.20.1-R0.1-SNAPSHOT") // Dependensi untuk PaperMC
     implementation(kotlin("stdlib"))
