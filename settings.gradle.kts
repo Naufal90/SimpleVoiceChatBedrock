c6643afc08021b748d42dc2b5b49e02c379d8cd3
Naufal90 <45502745+Naufal90@users.noreply.github.com>
2025-01-07 08:45:32 +0700

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,7 +9,7 @@ pluginManagement {
     }
     plugins {
         id("com.android.application") version "8.1.0" apply false  // Menambahkan `apply false` untuk memastikan hanya diterapkan di subproyek yang relevan
-        id("org.jetbrains.kotlin.android") version "1.7.10"
+        id("org.jetbrains.kotlin.android") version "1.7.10" apply false
     }
 }
 
